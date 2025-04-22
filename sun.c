5faa0e9f59e5da3d5e0c8201a605aa2527188321
sunitha <Sunitha.Bijjam@truminds.com>
2025-04-22 17:05:09 +0530

    my first official commit

---
+++ sun.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () 
+{
+  printf ("Welcome to linuxDD\n");
+  printf("This is my first code in linuxDD repository\n");
+  printf("I am sunitha Bijjam\n");
+  return 0;
+}
