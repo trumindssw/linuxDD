304b1785a5a7578a3ecd653399dd767622e1b0e4
naveen-karumanchi2001 <naveen.karumanchi@truminds.com>
2025-04-24 16:57:46 +0530

    NaveenAssignment1

--- write.c
+++ write.c
@@ -1,16 +1,12 @@
 #include "header.h"
-//Union for semaphore control
 union semun{
 int val;
 };
 int main()
 {
-int shmid=shmget(SHM_KEY, 1024,0666 | IPC_CREAT);//to create shared memory segment
-SharedData *data=(SharedData *)shmat(shmid,NULL,0); //attach to your process
-  // Create or get a semaphore (1 semaphore in the set)
+SharedData *data=(SharedData *)shmat(shmid,NULL,0); 
     int semid = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
  
-    // Initialize semaphore to 1 (unlocked state)
     union semun arg;
     arg.val = 1;
     semctl(semid, 0, SETVAL, arg);
@@ -26,17 +22,14 @@ SharedData *data=(SharedData *)shmat(shmid,NULL,0); //attach to your process
     printf("Enter a message to send: ");
     fgets(input, MAX_MESSAGE_LEN, stdin);
 
-    input[strcspn(input, "\n")] = '\0';  // Remove newline character.
+    input[strcspn(input, "\n")] = '\0';  
  
-    // Lock semaphore (enter critical section)
     semop(semid, &lock, 1);
  
-    // Write to shared memory
     strncpy(data->message, input, MAX_MESSAGE_LEN);
  
     // Unlock semaphore (leave critical section)
     semop(semid, &unlock, 1);
-// Log the message to shmem.txt
          FILE *log = fopen("shmem.txt", "a");
     fprintf(log, "Process ID: %d: %s\n", getpid(), input);
     fclose(log);
