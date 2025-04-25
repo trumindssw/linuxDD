a35f5f5c31884c24ca158e97a20775b3c1fc7d67
saivenkat-321 <Venkateswara.Guttula@truminds.com>
2025-04-25 10:16:11 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -2,25 +2,29 @@
 
 int main()
 {
-	
+	//shared memory creation
+
     int shmid = shmget(SHM_key, SIZE, IPC_CREAT | 0666);
     if (shmid == -1)
     {
 	    perror("shmget");
 	    exit(1);
     }
+	//attach shared memory
     struct Data *data = (struct Data *)shmat(shmid, NULL, 0);
     if (data == (void *)-1)
     {
 	    perror("shmat");
 	    exit(1);
     }
+	//semaphore creation
     int semid = semget(SEM_key, 1, IPC_CREAT | 0666);
     if (semid == -1)
     {
 	    perror("semget");
 	    exit(1);
     }
+	//set semaphore value
     semctl(semid, 0, SETVAL, 0);
     char msg2[1000];
     printf("Enter message to send to receiver: ");
@@ -32,6 +36,7 @@ int main()
     strcpy(data->msg, msg2);
     unlock_semaphore(semid);
     message1(data->msg);
+	//detach shared memory
     shmdt(data);
     printf("Message sent: %s\n", msg2);
     return 0;
